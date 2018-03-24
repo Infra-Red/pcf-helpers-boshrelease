5bf7673c5418a2c5b48dc77f643e4746b6232df8
Andrei Krasnitski <me@infra-red.xyz>
2018-03-24 10:11:46 -0600

    First commit

---
+++ jobs/bosh-dns-enable/templates/prepend-nameserver.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# If a command fails, exit immediately
+set -e
+
+prepend_nameserver() {
+  local CONFIG_FILE=$1
+  local NAMESERVER="169.254.0.2"
+
+  # prepend bosh dns server
+  echo -e "nameserver ${NAMESERVER}\n$(cat ${CONFIG_FILE})" > ${CONFIG_FILE}
+
+  # remove any duplicates, matters if this script gets executed multiple times
+  echo "$(uniq ${CONFIG_FILE})" > ${CONFIG_FILE}
+}
